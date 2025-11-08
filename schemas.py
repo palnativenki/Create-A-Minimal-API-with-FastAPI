24a2c1e07134efd5159907d4b94697f012bcc9e4
palnativenki <132834685+palnativenki@users.noreply.github.com>
2025-11-08 20:57:38 +0530

    schemas.py

--- schemas.py
+++ schemas.py
@@ -1,1 +1,19 @@
+from pydantic import BaseModel, Field, validator
+
+class AddressBase(BaseModel):
+    name: str = Field(..., min_length=1)
+    latitude: float = Field(..., ge=-90, le=90)
+    longitude: float = Field(..., ge=-180, le=180)
+
+class AddressCreate(AddressBase):
+    pass
+
+class AddressUpdate(AddressBase):
+    pass
+
+class AddressOut(AddressBase):
+    id: int
+
+    class Config:
+        orm_mode = True
 
